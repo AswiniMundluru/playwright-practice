ed64dcc61b2fe23999f03cc4d58d6995c9b61426
Aswini Mundluru <aswinimundluru@Aswinis-Laptop.local>
2026-09-18 09:10:24 +0530

    latest commit

--- tests/mousehover.spec.ts
+++ tests/mousehover.spec.ts
@@ -1,12 +1,40 @@
-import { Page,expect, test , Locator} from "@playwright/test"
+import { Page,expect, test , Locator, chromium} from "@playwright/test"
 
-test ( "mouse hover test" , async({page})=>{
+test ( "mouse hover test" , async()=>{
 
-await page.goto("https://www.spicejet.com/");
-await page.getByText("Add-ons").hover();
-await page.getByText("SpiceAssurance").click();
+    const broswer = await chromium.launch();
+    const browsercontext = await broswer.newContext();
+    const parentpage = await browsercontext.newPage();
+
+await parentpage.goto("https://www.spicejet.com/");
+await parentpage.getByText("Add-ons",{ exact: true}).first().hover();
+await parentpage.getByText("SpiceAssurance").click();
+
+try {//same page
+
+    await parentpage.waitForURL("/SpiceAssurance/");
+    await expect(parentpage).toHaveURL("/SpiceAssurance/");
+}
+
+catch{
+//new page
+
+const allpages =  browsercontext.pages();
+
+for( const newpage of allpages){
+
+    const otherpage =  newpage.url();
 
 
+    if( newpage!==parentpage &&  otherpage.includes("SpiceAssurance"))
+        {
 
+        console.log("newpage opened");
+
+    }
+}
+
+
+}
 
 });
